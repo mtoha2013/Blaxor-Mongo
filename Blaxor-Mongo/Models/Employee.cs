cf30164b5cf7f732520444cd9087d8f8374b8677
Mudzakkir Toha <mymail.mth@gmail.com>
2023-06-18 16:27:37 +0700

    first commit

---
+++ Blaxor-Mongo/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blaxor_Mongo.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        public object Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
